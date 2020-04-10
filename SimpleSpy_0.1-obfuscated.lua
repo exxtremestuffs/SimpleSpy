7ed9c65292568f871483706ac7ffe0cfb4602cd1
exxtremestuffs <jheinemann@protonmail.ch>
2020-04-09 17:38:23 -0700

    Update SimpleSpy_0.1-obfuscated.lua

--- SimpleSpy_0.1-obfuscated.lua
+++ SimpleSpy_0.1-obfuscated.lua
@@ -1,2 +1,1 @@
-game:GetService("StarterGui"):SetCore("SendNotification", "LOADSTRING OUTDATED!", "The loadstring you are using is outdated! Please get the updated one from V3rmillion.")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua"))()
